70a9581964ea6922e0d3eb47da5f6d8c08d5c131
maryrmds <rms.mariana1@gmail.com>
2023-11-17 18:01:20 -0300

    Primeira execução do servidor

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const express = require(express)
+const express = require("express")
 const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Hello World!")
+})
+
+app.listen (3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
