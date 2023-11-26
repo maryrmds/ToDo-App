c7406453da233b935b2973fd0be455dbb0f8e471
maryrmds <rms.mariana1@gmail.com>
2023-11-25 21:22:52 -0300

    Iniciando a conexão com o MySQL

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const mysql = require("mysql2")
 
 const app = express()
 
@@ -12,6 +13,12 @@ app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
 
-app.listen (3000, () => {
-    console.log("Servidor rodando na porta 3000")
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "3306",
+    password: "3306",
+    database: "todoapp",
+    port: 3306
+
 })
+
