3d5793ea064f6dc7351adf513797b755621f72f4
maryrmds <rms.mariana1@gmail.com>
2023-11-28 21:28:21 -0300

    Filtro de tarefas ativo completa

--- index.js
+++ index.js
@@ -76,7 +76,28 @@ app.post('/criar', (requisicao, resposta) => {
 })
 
 app.get('/ativas', (requisicao, resposta) => {
+    const sql = `
+    SELECT * FROM tarefas
+    WHERE completa = 0;
+    `
 
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dados) => {
+            return {
+                id:dados.id,
+                descricao: dados.descricao,
+                completa: false
+            }
+        })
+
+        const quantidadeTarefas = tarefas.length
+
+        resposta.render('ativas', { tarefas, quantidadeTarefas })
+    })
 })
 
 app.get('/', (requisicao, resposta) => {
@@ -89,9 +110,9 @@ app.get('/', (requisicao, resposta) => {
 
         const tarefas = dados.map((dados) => {
             return {
-                id:dados.id,
+                id: dados.id,
                 descricao: dados.descricao,
-                completa:dados.completa === 0 ? false : true
+                completa: dados.completa === 0 ? false : true
             }
         })
 
